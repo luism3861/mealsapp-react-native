9d6c5f0756c2738a28652017b15535bceb6d5fdd
Luis Medina <luism3861@gmail.com>
2019-10-13 19:52:55 -0500

    added all the rest model mealsScreen

--- src/screens/CategoryMealsScreen.js
+++ src/screens/CategoryMealsScreen.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
-import {View, FlatList} from 'react-native';
+import {FlatList} from 'react-native';
 import {CATEGORIES, MEALS} from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = ({navigation}) => {
   const renderMealItem = itemData => {
     return (
-      <View>
-        <Text>{itemData.item.title}</Text>
-      </View>
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          navigation.navigate('MealDetail', {
+            mealId: itemData.item.id,
+          });
+        }}
+      />
     );
   };
 
@@ -40,10 +50,8 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  padding: 15px;
+  margin-bottom: 10px;
 `;
-
-const Text = styled.Text``;
-
-const Button = styled.Button``;
 
 export default CategoryMealScreen;
